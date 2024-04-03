ebcec2ed7b3a56e253c5ad06b2b5a0228cb38ab1
andy_ <dac.ngm89@gmail.com>
2024-04-03 10:13:01 +0700

    set original image

--- util/util.js
+++ util/util.js
@@ -20,9 +20,6 @@ export async function filterImageFromURL(inputURL) {
             const outpath =
                 "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
             await photo
-                .resize(256, 256) // resize
-                .quality(60) // set JPEG quality
-                .greyscale() // set greyscale
                 .write(outpath, (img) => {
                     resolve(outpath);
                 });
